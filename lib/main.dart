623628c624639eb0fae6a0d60f95cf8258875c02
meikub <44545393+meikub@users.noreply.github.com>
2021-10-01 15:08:12 +0200

    74->4:45

--- lib/main.dart
+++ lib/main.dart
@@ -5,26 +5,8 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget{
-  @override
-  State<StatefulWidget> createState() {
-    return MyAppState();
-  }
-}
+class MyApp extends StatelessWidget {
 
-class MyAppState extends State<MyApp> {
-  int questionIndex = 0;
-  var questions = ['question1?', 'question2?', 'question3?'];
-
-  void answerQuestion() {
-    setState(() {
-      questionIndex++;
-    });
-    print(questions[questionIndex]);
-  }
-
-  //build ist so ähnlich wie der Update-LifeCycle in Unity. Unterschied ist, dass Update pro Frame aufgerufen wird und build ändert sich, sobald ein Trigger auf einen unterschied hinweist
-  // Änderung im UI => rebuild App
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,9 +14,10 @@ class MyAppState extends State<MyApp> {
             appBar: AppBar(title: Text('Hello')),
             body: Column(
               children: [
-                Text(questions[questionIndex]),
-                Text("two"),
-                RaisedButton(onPressed: answerQuestion)
+                Card(
+                    color: Colors.blueGrey,
+                    child: Container(width: double.infinity,child: Text('Chart'))),
+                Card(child: Text('List of Transactions'))
               ],
             )));
   }
