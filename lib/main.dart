c288301cb66a09c39f9f52da25cbc3379d260be5
meikub <44545393+meikub@users.noreply.github.com>
2021-10-08 15:04:22 +0200

    100->0:00

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tutorial/widgets/user_transactions.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_tutorial/widgets/new_transactions.dart';
+import 'package:flutter_tutorial/widgets/transaction_list.dart';
+
+import 'models/transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,18 +13,83 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(title: Text('Hello')),
-            body: Column(
-              // mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                    width: double.infinity,
-                    child: const Card(
-                        color: Colors.blueGrey, child: Text('Chart'))),
-                UserTransactions(),
-              ],
-            )));
+      title: 'Flutter App',
+      home: MyHomePage(),
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+              .copyWith(secondary: Colors.pinkAccent),
+          fontFamily: 'Quicksand',
+          textTheme: ThemeData.light().textTheme.copyWith(
+            headline6: const TextStyle(fontFamily: 'OpenSans',fontWeight: FontWeight.bold, fontSize: 14)
+          ),
+          appBarTheme: const AppBarTheme(
+            titleTextStyle: TextStyle(fontFamily: 'OpenSans', fontSize: 20, fontWeight: FontWeight.bold),
+          )),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePage();
+}
+
+class _MyHomePage extends State<MyHomePage> {
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            child: NewTransaction(_addNewTransaction),
+            onTap: () {},
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
+
+  final List<Transaction> _userTransaction = [];
+
+  void _addNewTransaction(String title, double amount) {
+    final newTx = Transaction(
+        id: DateTime.now().toString(),
+        title: title,
+        amount: amount,
+        date: DateTime.now());
+    setState(() {
+      _userTransaction.add(newTx);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hello'),
+        actions: [
+          IconButton(
+              onPressed: () => _startAddNewTransaction(context),
+              icon: Icon(Icons.add))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+                width: double.infinity,
+                child: Card(
+                    color: Theme.of(context).primaryColor,
+                    child: Text('Chart'))),
+            TransactionList(_userTransaction),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
+      ),
+    );
   }
 }
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,38 +9,56 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: transactions.map((tx) {
-          return Card(
-              child: Row(
+    return Container(
+        height: 300,
+        child: transactions.isEmpty
+            ? Column(
                 children: [
+                  Text(
+                    'No Transactions added yet',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  SizedBox(height: 20,),
                   Container(
-                    margin: const EdgeInsets.symmetric(
-                        vertical: 10.0, horizontal: 15.0),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.purple, width: 2)),
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text('\$${tx.amount}',
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.purple)),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tx.title,
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        DateFormat().add_yMd().format(tx.date),
-                        style: TextStyle(color: Colors.grey),
-                      )
-                    ],
-                  ),
+                      height: 200,child: Image.asset('assets/imgs/waiting.png', fit: BoxFit.cover,))
                 ],
+              )
+            : ListView.builder(
+                itemBuilder: (ctx, index) {
+                  return Card(
+                      child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 15.0),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.purple, width: 2)),
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                            '\$${transactions[index].amount.toStringAsFixed(2)}',
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.purple)),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            transactions[index].title,
+                            style: Theme.of(context).textTheme.headline6,
+                          ),
+                          Text(
+                            DateFormat()
+                                .add_yMd()
+                                .format(transactions[index].date),
+                            style: TextStyle(color: Colors.grey),
+                          )
+                        ],
+                      ),
+                    ],
+                  ));
+                },
+                itemCount: transactions.length,
               ));
-        }).toList());
   }
 }
